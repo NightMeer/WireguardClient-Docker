e672eb971966c08d787cad3656869408c3bf0598
NightMeer <44423095+NightMeer@users.noreply.github.com>
2023-11-13 06:55:31 +0100

    Add $nic to set nic name in conf

--- startup.sh
+++ startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+cp /config/wg0.conf /etc/wireguard/wg0.conf
+
+nic=$(ip link | awk -F: '$0 !~ "lo|vir|wl|wg|^[^0-9]"{print $2;getline}')
+sed -i "s/_nic/$nic/g" /etc/wireguard/wg0.conf
+
 wg-quick up wg0
 
 tail -f /dev/null
